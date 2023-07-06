9cb2d60e7924dfa73266efd769e75ba85f317856
moleNULL <94998782+moleNULL@users.noreply.github.com>
2023-07-06 22:16:50 +0300

    Added entities for Book, Librarian, Student and BookManager

---
+++ LibraryManagementSystem.BLL/Models/Entities/BookEntities/PublisherEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementSystem.BLL.Models.Entities.BookEntities
+{
+    public class PublisherEntity
+    {
+        public int Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public ICollection<BookEntity> Books { get; init; } = new List<BookEntity>();
+    }
+}
