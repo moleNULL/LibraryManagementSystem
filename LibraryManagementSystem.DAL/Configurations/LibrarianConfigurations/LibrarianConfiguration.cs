a336e281bd2aa8f5ba61207a6039421dcf0d1db8
moleNULL <94998782+moleNULL@users.noreply.github.com>
2023-07-08 15:27:49 +0300

    Added configurations to several entities

---
+++ LibraryManagementSystem.DAL/Configurations/LibrarianConfigurations/LibrarianConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using LibraryManagementSystem.BLL.Models.Entities.LibrarianEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LibraryManagementSystem.DAL.Configurations.LibrarianConfigurations
+{
+    public class LibrarianConfiguration : IEntityTypeConfiguration<LibrarianEntity>
+    {
+        public void Configure(EntityTypeBuilder<LibrarianEntity> builder)
+        {
+            builder.ToTable("Librarians");
+
+            builder.HasKey(l => l.Id);
+
+            builder.Property(l => l.FirstName).HasMaxLength(75);
+            builder.Property(l => l.LastName).HasMaxLength(75);
+            builder.Property(l => l.Email).HasMaxLength(75);
+
+            builder.Property(l => l.PictureName).HasMaxLength(75);
+
+        }
+    }
+}
